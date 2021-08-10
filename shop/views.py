ea1dbef60a7696c677c0ba133f423dc7a1d307cc
thabsheera-banu <banuthabsheera@gmail.com>
2021-08-10 20:58:48 +0400

    second commit

--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from .models import *
 
-# Create your views here.
+# Create your views here.dfdfdfdsdc
+
 def home(request,c_slug=None):
     c_page=None
     prodt=None
