7518afcc2012f8d45fd3243048b35f51ae17e459
mubashir-cvr <mubashircheruvayoor@gmail.com>
2021-08-11 15:25:27 +0530

    Umcommented

--- shop/urls.py
+++ shop/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns=[
     path('',views.home,name='hm'),
-   # path('<slug:c_slug>/',views.home,name='prod_cat'),
+    path('<slug:c_slug>/',views.home,name='prod_cat'),
 
 ]
--- shop/views.py
+++ shop/views.py
@@ -4,15 +4,15 @@ from .models import *
 # Create your views here.dfdfdfdsdc
 
 def home(request,c_slug=None):
-    c_page=None
     prodt=None
-    if c_page!=None:
-        #c_page=get_object_or_404(categ,slug=c_slug)
+    c_page=None
+    if c_slug!=None:
+        c_page=get_object_or_404(categ,slug=c_slug)
         prodt=products.objects.filter(category=c_page,available=True)
     else:
         prodt = products.objects.all().filter(available=True)
-        cat = categ.objects.all()
-        return render(request, 'index.html', {'pr': prodt, 'ct': cat})
+    cat = categ.objects.all()
+    return render(request, 'index.html', {'pr': prodt, 'ct': cat})
 
 
 
